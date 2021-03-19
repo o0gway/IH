cd5fa1f597698af87385b5b8c159b533bf7f466a
wenzel <kachman87@gmail.com>
2021-03-19 20:33:35 +0500

    Added lesson2

---
+++ lesson02/main.py
@@ -0,0 +1,21 @@
+def parse(query: str) -> dict:
+    pass
+
+
+def parse_cookie(query: str) -> dict:
+    pass
+
+
+if __name__ == '__main__':
+    # parse
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
+    # parse_cookie
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
